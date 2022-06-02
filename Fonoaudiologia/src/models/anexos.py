7722400607bbc94c95753a2cd284e19833818198
Alejo <alejito23001@gmail.com>
2022-06-02 11:05:02 -0500

    DELETE: Fonoaudiologia eliminado porque, como no estaba completo, quedara como opcional

--- Fonoaudiologia/src/models/anexos.py
+++
@@ -1,3 +0,0 @@
-from src.services.anexos_service import AnexosService
-
-anexos = AnexosService()
--- Fonoaudiologia/src/utils/instances.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_redis import FlaskRedis
-from redis import Redis
-
-db: SQLAlchemy = SQLAlchemy()
-rd: Redis = FlaskRedis(decode_responses=True)
